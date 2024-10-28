48a505422ee1a9c6859af0cd0e04d1a5d3b4b5f7
tomeryoel <Tomerys@gmail.com>
2024-10-28 10:05:45 +0200

    changing the baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080', 
+    baseURL: 'https://tomashmovierecommendationapis.onrender.com', 
     headers: { "ngrok-skip-browser-warning": "true" }
 });
+
+// export default axios.create({
+//     baseURL: 'http://localhost:8080', 
+//     headers: { "ngrok-skip-browser-warning": "true" }
+// });
 
 // export default axios.create({
 //     baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
